1b8e9aa31ff036fb15080bd965b747b5f1b5cf28
Aviral Srivastava <aviralji4@gmail.com>
2023-01-05 10:26:54 +0530

    Update burp_py.py

--- burp_py.py
+++ burp_py.py
@@ -3,6 +3,7 @@ from pprint import pprint
 import os
 import json
 
+
 def parse_request(file_name):
     line = ""
     headers = {}
@@ -16,9 +17,8 @@ def parse_request(file_name):
             if line.startswith("\n"):
                 header_collection_done = True
             else:
-                headers.update({
-                    line[0:line.find(":")].strip(): line[line.find(":")+1:].strip()
-                })
+                headers.update(
+                    {line[0:line.find(":")].strip(): line[line.find(":")+1:].strip()})
         else:
             post_data = post_data + line
     file_object.close()
@@ -30,14 +30,15 @@ for filename in os.scandir(directory):
     if filename.is_file():
         print("Reading File - ", filename.path)
         header, post_data = parse_request(filename.path)
-        json_header = json.dumps(header, indent=4)
-        json_header = json.loads(json_header)
+        # json_header = json.dumps(header, indent=4)
+        # json_header = json.loads(json_header)
         json_post_data = json.dumps(post_data, indent=4)
         json_post_data = json.loads(json_post_data)
-        pprint(json_header)
-        pprint(json_post_data)
+        dict = json.loads(json_post_data)
+        # pprint(json_header)
+        print(dict)
         print()
-        with open("output_files/"+filename.name+"_headers.json", "w") as outfile:
-            json.dump(json_header, outfile)
-        with open("output_files/"+filename.name+"_data.json", "w") as outfile:
-        	json.dump(json_post_data, outfile)
+        # with open("output_files/"+filename.name+"_headers.json", "w") as outfile:
+        #     json.dump(json_header, outfile)
+        # with open("output_files/"+filename.name+"_data.json", "w") as outfile:
+        #     json.dump(json_post_data, outfile)
